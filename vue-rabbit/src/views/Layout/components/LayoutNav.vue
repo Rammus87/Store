d652cfa87f36819030aabbbea28058c3060ccc35
Rammus87 <131853744+Rammus87@users.noreply.github.com>
2023-06-06 12:46:33 +0800

    引入圖標字體

---
+++ vue-rabbit/src/views/Layout/components/LayoutNav.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="app-topnav">
+        <div class="container">
+            <ul>
+                <template v-if="true">
+                    <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰倫</a></li>
+                    <li>
+                        <el-popconfirm title="確認退出嗎？" confirm-button-text="確認" concel-button="取消">
+                            <template #reference>
+                                <a href="javascript:;">退出登錄</a>
+                            </template>
+                        </el-popconfirm>
+                    </li>
+                    <li><a href="javascript:;">我的訂單</a></li>
+                    <li><a href="javascript:;">會員中心</a></li>
+                </template>
+                <template v-else>
+                    <li><a href="javascript:;">請先登錄</a></li>
+                    <li><a href="javascript:;">幫助中心</a></li>
+                    <li><a href="javascript:;">關於我們</a></li>
+                </template>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+.app-topnav{
+    background: #333;
+
+    ul{
+        display: flex;
+        height: 53px;
+        justify-content: flex-end;
+        align-items: center;
+
+        li{
+            a{
+                padding: 0 15px;
+                color: #cdcdcd;
+                line-height: 1;
+                display: inline-block;
+
+                i{
+                    font-size: 14px;
+                    margin-right: 2px;
+                }
+
+                &:hover {
+                    color: $xtxColor;
+                }
+            }
+
+            ~li{
+                a{
+                    border-left: 2px solid #666;
+                }
+            }
+        }
+    }
+}
+</style>
